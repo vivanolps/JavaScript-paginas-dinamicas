c3f9447dc08c87908ac677d9271b35751dbde64a
Evelyn Lima <vinhahh.3000@gmail.com>
2025-05-21 11:27:13 -0300

    Anexei elementos, do mas novo para o mas velho.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const inputItem = document.getElementById("input-item")
-
+const inputItem = document.getElementById("input-item");
+const listaDeCompras = document.getElementById("lista-de-compras");
 const botaoAdicionar = document.getElementById("adicionar-item");
 let contador = 0;
 
@@ -16,4 +16,13 @@ botaoAdicionar.addEventListener("click", (evento) => {   //função de retorno
     const inputCheckbox = document.createElement("input");
     inputCheckbox.type = "checkbox";
     inputCheckbox.id = "checkbox-" + contador++;
+    const nomeItem = document.createElement("p");
+    nomeItem.innerText = inputItem.value;
+
+    containerItemDaLista.appendChild(inputCheckbox);
+    containerItemDaLista.appendChild(nomeItem);
+
+    itemDaLista.appendChild(containerItemDaLista);
+    listaDeCompras.appendChild(itemDaLista);
+
 })
