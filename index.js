01f642d8240694303518082ab704b4d2b27352f9
Evelyn Lima <vinhahh.3000@gmail.com>
2025-05-21 11:14:20 -0300

    Usei a variavel let, adicionei o checkbox.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inputItem = document.getElementById("input-item")
 
 const botaoAdicionar = document.getElementById("adicionar-item");
+let contador = 0;
 
 botaoAdicionar.addEventListener("click", (evento) => {   //função de retorno
    evento.preventDefault();
@@ -12,4 +13,7 @@ botaoAdicionar.addEventListener("click", (evento) => {   //função de retorno
     const itemDaLista = document.createElement("li");
     const containerItemDaLista = document.createElement("div");
     containerItemDaLista.classList.add("lista-item-container");
+    const inputCheckbox = document.createElement("input");
+    inputCheckbox.type = "checkbox";
+    inputCheckbox.id = "checkbox-" + contador++;
 })
