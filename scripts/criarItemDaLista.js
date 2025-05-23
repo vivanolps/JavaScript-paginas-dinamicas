0eb7b0c2bb1116507501fef2296bfda5ae58b12e
Evelyn Lima <vinhahh.3000@gmail.com>
2025-05-23 09:30:03 -0300

    Export default

--- scripts/criarItemDaLista.js
+++ scripts/criarItemDaLista.js
@@ -1,3 +1,4 @@
+import gerarDiaDaSemana from "./gerarDiaDaSemana.js";
 const inputItem = document.getElementById("input-item");
 let contador = 0;
 
@@ -30,16 +31,9 @@ inputCheckbox.addEventListener("click", function() {
     containerItemDaLista.appendChild(nomeItem);
 
     itemDaLista.appendChild(containerItemDaLista);
+     const dataCompleta = gerarDiaDaSemana();
 
-
-        const diaDaSemana = new Date().toLocaleDateString("pt-BR", {
-        weekday: "long"});
-        const data = new Date().toLocaleDateString("pt-BR")
-        const hora = new Date().toLocaleTimeString("pt-BR", {
-            hour: "numeric",
-            minute: "numeric"
-        });
-        const  dataCompleta = `${diaDaSemana} (${data}) às ${hora}`
+      
         const itemData = document.createElement("p");
         itemData.innerText = dataCompleta;
         itemData.classList.add("texto-data")
